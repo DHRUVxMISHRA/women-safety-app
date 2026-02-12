ab05d2b3eb15ba5b1b547048beb04424143d625d
Mohammad Sohail Ali <145998001+MdSohailAli3@users.noreply.github.com>
2026-02-12 11:50:17 +0530

    Delete backend/app directory

--- backend/app/schemas/chat_schema.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-# validates the request user made
-# check types, if type mismatch -> Fastapi responds - 422 Unprocessable Entity
-
-class ChatRequest(BaseModel):
-    message: str
